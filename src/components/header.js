9d9ee832474e42f67400fee5c181ddc7955ab7ed
poojagunturu96 <pgunturu@middlebury.edu>
2023-01-23 15:12:52 -0600

    Updated logo image path to use withPrefix to prepend path prefix in production

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,15 @@
 import React from "react";
 import MiddLogo from "../images/midd-shield.svg";
+import { withPrefix } from "gatsby";
 
 const Header = () => {
   return (
     <header className="app-header">
       <div className="app-header__wrap app-header__wrap--main">
         <div className="app-header__logo">
-          <a href="/">
+          <a href="/handbook">
             <img
-              src={MiddLogo}
+              src={withPrefix(MiddLogo)}
               sizes="(min-width: 1024px) 67px, 44px"
               alt="Middlebury Shield"
             />
