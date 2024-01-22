7b1e009e956b8386575d4dcbe6bb6ae97d6baa4d
poojagunturu96 <pgunturu@middlebury.edu>
2024-01-22 11:48:55 -0600

    update /handbook url in js files

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,12 @@
 import React from "react";
 import MiddLogo from "../images/midd-shield.svg";
-// import { withPrefix } from "gatsby";
 
 const Header = () => {
   return (
     <header className="app-header">
       <div className="app-header__wrap app-header__wrap--main">
         <div className="app-header__logo">
-          <a href="/handbook">
+          <a href="/">
             <img
               src={MiddLogo}
               sizes="(min-width: 1024px) 67px, 44px"
@@ -17,7 +16,7 @@ const Header = () => {
         </div>
 
         <h1 className="app-header__title" id="midd-app-header-label">
-          <a href="/handbook" className="app-header__link">
+          <a href="/" className="app-header__link">
             Middlebury Handbook
           </a>
         </h1>
