16f291c1f23ad5a78a6d042c2117ced873604ad1
poojagunturu96 <pgunturu@middlebury.edu>
2024-01-23 09:55:38 -0600

    Revert "update /handbook url in js files"
    This reverts commit 7b1e009e956b8386575d4dcbe6bb6ae97d6baa4d.

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import MiddLogo from "../images/midd-shield.svg";
+// import { withPrefix } from "gatsby";
 
 const Header = () => {
   return (
     <header className="app-header">
       <div className="app-header__wrap app-header__wrap--main">
         <div className="app-header__logo">
-          <a href="/">
+          <a href="/handbook">
             <img
               src={MiddLogo}
               sizes="(min-width: 1024px) 67px, 44px"
@@ -16,7 +17,7 @@ const Header = () => {
         </div>
 
         <h1 className="app-header__title" id="midd-app-header-label">
-          <a href="/" className="app-header__link">
+          <a href="/handbook" className="app-header__link">
             Middlebury Handbook
           </a>
         </h1>
