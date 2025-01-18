b6507208cf47975bb4b2c7876a610f53acc1a74d
poojagunturu96 <pgunturu@middlebury.edu>
2025-01-17 18:48:13 -0600

    update the way netlify identity is loaded

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Homepage from "../components/homepage";
+import netlifyIdentity from 'netlify-identity-widget';
+
+window.netlifyIdentity = netlifyIdentity;
+netlifyIdentity.init();
 
 const IndexPage = () => {
   return <Homepage></Homepage>;
